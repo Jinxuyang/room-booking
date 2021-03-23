d881997a68330479778eb6aca7425c5c255f1651
jinxuyang <jinxuyang3@gmail.com>
2021-03-23 21:18:57 +0800

    feat<oauth2>: connect to nacos

--- room-booking-oauth2/src/main/java/com/fehead/roomBooking/oauth2/OAuth2Application.java
+++ room-booking-oauth2/src/main/java/com/fehead/roomBooking/oauth2/OAuth2Application.java
@@ -3,6 +3,7 @@ package com.fehead.roomBooking.oauth2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author Verge
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.fehead.roomBooking.oauth2.mapper")
+@EnableDiscoveryClient
 public class OAuth2Application {
     public static void main(String[] args) {
         SpringApplication.run(OAuth2Application.class,args);
