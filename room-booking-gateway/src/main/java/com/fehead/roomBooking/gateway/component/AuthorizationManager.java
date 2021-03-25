9b079485104a5b072bea443dda12a2490983fb5d
jinxuyang <jinxuyang3@gmail.com>
2021-03-25 19:29:05 +0800

    feat<gateway>: add new module

---
+++ room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/AuthorizationManager.java
@@ -0,0 +1,42 @@
+package com.fehead.roomBooking.gateway.component;
+
+import cn.hutool.core.convert.Convert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author Verge
+ * @Date 2021/3/24 19:43
+ * @Version 1.0
+ */
+@Component
+public class AuthorizationManager implements ReactiveAuthorizationManager<AuthorizationContext> {
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> mono, AuthorizationContext authorizationContext) {
+        URI uri = authorizationContext.getExchange().getRequest().getURI();
+        Object obj = redisTemplate.opsForHash().get("AUTH:RESOURCE_ROLES_MAP", uri.getPath());
+        List<String> authorities = Convert.toList(String.class,obj);
+        authorities = authorities.stream().map(i -> i = "ROLE_" + i).collect(Collectors.toList());
+        return mono
+                .filter(Authentication::isAuthenticated)
+                .flatMapIterable(Authentication::getAuthorities)
+                .map(GrantedAuthority::getAuthority)
+                .any(authorities::contains)
+                .map(AuthorizationDecision::new)
+                .defaultIfEmpty(new AuthorizationDecision(true));
+    }
+}
