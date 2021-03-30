2d012a52ed55ac3124a1bcb943d943fd861ffef5
Verge <981340404@qq.com>
2021-03-30 22:04:16 +0800

    fix<gateway>: asfasedgfsdfhg

--- room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/RestAuthenticationEntryPoint.java
+++ room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/RestAuthenticationEntryPoint.java
@@ -25,9 +25,9 @@ public class RestAuthenticationEntryPoint implements ServerAuthenticationEntryPo
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
         ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.OK);
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        String body= JSONUtil.toJsonStr(CommonReturnType.create(e.getMessage()));
+        String body= JSONUtil.toJsonStr(CommonReturnType.create(e.getMessage(),"failed"));
         DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(StandardCharsets.UTF_8));
         return response.writeWith(Mono.just(buffer));
     }
--- room-booking-oauth2/src/main/java/com/fehead/roomBooking/oauth2/config/WebSecurityConfig.java
+++ room-booking-oauth2/src/main/java/com/fehead/roomBooking/oauth2/config/WebSecurityConfig.java
@@ -47,6 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
+
         return super.authenticationManagerBean();
     }
 }
