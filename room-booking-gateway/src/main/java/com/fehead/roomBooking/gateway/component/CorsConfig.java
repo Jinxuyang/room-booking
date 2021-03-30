dd18cf2911db47cb8aaa7137926d7c1213214629
jinxuyang <jinxuyang3@gmail.com>
2021-03-30 19:23:34 +0800

    fix<gateway>: fix cors

---
+++ room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.fehead.roomBooking.gateway.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author Verge
+ * @Date 2021/3/29 20:42
+ * @Version 1.0
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
