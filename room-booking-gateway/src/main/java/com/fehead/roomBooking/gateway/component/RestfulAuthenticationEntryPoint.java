b5ca48b7898cf891a637a16154d3f5c647244a30
Verge <981340404@qq.com>
2021-03-31 13:24:57 +0800

    fix<oauth2>

--- room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/RestfulAuthenticationEntryPoint.java
+++ room-booking-gateway/src/main/java/com/fehead/roomBooking/gateway/component/RestfulAuthenticationEntryPoint.java
@@ -6,20 +6,14 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.provider.error.DefaultWebResponseExceptionTranslator;
-import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Author Verge
@@ -30,7 +24,7 @@ import java.util.Map;
 public class RestfulAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
-        WebResponseExceptionTranslator<?> exceptionTranslator = new DefaultWebResponseExceptionTranslator();
+        /*WebResponseExceptionTranslator<?> exceptionTranslator = new DefaultWebResponseExceptionTranslator();
         try {
             ResponseEntity<?> result = exceptionTranslator.translate(e);
             if (result.getStatusCode() == HttpStatus.UNAUTHORIZED){
@@ -42,7 +36,7 @@ public class RestfulAuthenticationEntryPoint implements ServerAuthenticationEntr
 
         } catch (Exception exception) {
             exception.printStackTrace();
-        }
+        }*/
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
