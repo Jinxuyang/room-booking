a0d331d1480ed320e26a826a3fc3c71fd80b57b5
Verge <981340404@qq.com>
2021-04-05 21:57:32 +0800

    fix<global>: fix some bugs

--- room-booking-admin/src/main/java/com/fehead/roomBooking/admin/service/ApplicationService.java
+++ room-booking-admin/src/main/java/com/fehead/roomBooking/admin/service/ApplicationService.java
@@ -9,7 +9,6 @@ import com.fehead.roomBooking.common.entity.Application;
 import com.fehead.roomBooking.common.entity.ApplicationReturnType;
 import com.fehead.roomBooking.common.entity.RoomStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
---
+++ room-booking-common/src/main/java/com/fehead/roomBooking/common/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.fehead.roomBooking.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Verge
+ * @Date 2021/4/5 21:30
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class,args);
+    }
+}
